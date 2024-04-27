01d6cccdf20537ec0b03a31e67303e1d8cfb3352
idasso <idasso@fi.uba.ar>
2024-04-27 15:41:51 +0200

    Agregado de vista de todas las mediciones de la BD en pantalla

--- app-dam/src/frontend/dam/src/app/mediciones/mediciones.page.ts
+++ app-dam/src/frontend/dam/src/app/mediciones/mediciones.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MedicionService } from '../services/medicion.service';
+import { Medicion } from '../interfaces/medicion';
 
 
 
@@ -10,11 +11,14 @@ import { MedicionService } from '../services/medicion.service';
 })
 export class MedicionesPage implements OnInit {
 
+  listadoMediciones: Medicion[] | undefined
+
   constructor(private _medicionService: MedicionService) { }
 
   async ngOnInit() {
     await this._medicionService.getMediciones()
       .then((mediciones) => {
+        this.listadoMediciones = mediciones
         console.log(mediciones)
       })
       .catch((error) => {
